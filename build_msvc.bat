fd66037da631859ee43f4235d7407476fa1ba95d
ColaxDev <colax.dev@gmail.com>
2022-06-30 15:04:14 +0000

    ci: github

---
+++ build_msvc.bat
@@ -0,0 +1,19 @@
+@echo off
+rem launch this from msvc-enabled console
+
+
+set CFLAGS=/std:c11 /O2 /FC /W4 /WX /wd4996 /nologo
+set LIBS=
+
+set EXAMPLES=./examples/fib.har ./examples/123i.har ./examples/123f.har ./examples/e.har
+
+cl.exe %CFLAGS% ./src/hackc.c
+
+cl.exe %CFLAGS% ./src/hack.c
+
+cl.exe %CFLAGS% ./src/dehack.c
+
+if "%1" == "examples" setlocal EnableDelayedExpansion && for %%e in (%EXAMPLES%) do (
+    set name=%%e
+    "./hackc.exe" !name:~0,-2!hack %%e
+)
