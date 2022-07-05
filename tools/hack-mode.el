30ba1936c252a131c6aa938fb9504e98de554ed3
ColaxDev <colax.dev@gmail.com>
2022-07-05 08:50:31 +0000

    feat: labels

--- tools/hack-mode.el
+++ tools/hack-mode.el
@@ -11,7 +11,8 @@
         syn-table))
 
 (setq hack-highlights
-    '(("nop\\|push\\|drop\\|dup\\|plusi\\|minusi\\|multi\\|divi\\|modi\\|plusf\\|minusf\\|multf\\|divf\\|jmp_if\\|jmp\\|eq\\|halt\\|swap\\|not\\|gef\\|gei\\|ret\\|call\\|native\\|andb\\|orb\\|xor\\|shr\\|shl\\|notb\\|read8\\|read16\\|read32\\|read64\\|write8\\|write16\\|write32\\|write64" . font-lock-keyword-face)))
+    '(("[[:word:]_]+\\:" . font-lock-constant-face)
+      ("nop\\|push\\|drop\\|dup\\|plusi\\|minusi\\|multi\\|divi\\|modi\\|plusf\\|minusf\\|multf\\|divf\\|jmp_if\\|jmp\\|eq\\|halt\\|swap\\|not\\|gef\\|gei\\|ret\\|call\\|native\\|andb\\|orb\\|xor\\|shr\\|shl\\|notb\\|read8\\|read16\\|read32\\|read64\\|write8\\|write16\\|write32\\|write64" . font-lock-keyword-face)))
 
 (define-derived-mode hack-mode fundamental-mode "hack"
     "Major mode for editing Hack Assembly code."
