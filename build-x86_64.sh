cc51a39d45a33a41fd005595cccf4b946ff117f4
ColaxDev <colax.dev@gmail.com>
2022-07-13 13:12:21 +0000

    Add TODO(#37)

--- build-x86_64.sh
+++ build-x86_64.sh
@@ -11,3 +11,5 @@ ld -o ./examples/123i.exe ./examples/123i.o
 ./hack2nasm ./examples/fib.hack > ./examples/fib.asm
 nasm -felf64 -g ./examples/fib.asm -o ./examples/fib.o
 ld -o ./examples/fib.exe ./examples/fib.o
+
+# TODO(#37): not all of the examples are translatable with hack2nasm
